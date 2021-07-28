a505afb8d3c848ae8ebd2b0e9c94ecd2e7490361
MilanTagline2021 <85214636+MilanTagline2021@users.noreply.github.com>
2021-07-28 14:52:34 +0530

    Update item.py

--- app/backend/item.py
+++ app/backend/item.py
@@ -1,9 +1,1 @@
-from sqlalchemy.orm.session import Session
-from .. import models,schema
-
-def create(request:schema.BaseItem,db:Session):
-    new_obj = models.Item(title=request.title,description=request.description)
-    db.add(new_obj)
-    db. commit()
-    db.refresh(new_obj)
-    return new_obj   
+   
